cacbcf850edd7b18667e89c4ad5869160f616d62
waly2020 <walyguema@gmail.com>
2022-07-08 18:10:58 +0100

    css

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
 let menu = document.querySelector(".menu");
-
+const back_para = document.querySelectorAll(".back_para");
 
 window.addEventListener("scroll", () =>{
+
     let srlY = scrollY;
+
+    anime_back_para(srlY);
+
     if (srlY > 40){
         menu.classList.add("active");
     }
@@ -10,3 +14,10 @@ window.addEventListener("scroll", () =>{
         menu.classList.remove("active");
     }
 })
+
+function anime_back_para(scrollY){
+    back_para.forEach(para => {
+        console.log(para.getBoundingClientRect().top,scrollY);
+    })
+}
+anime_back_para(scrollY);
